656c25eb82cd9bf997267c606ba3352fe4ad7139
Tmdsb941 <1927919399@qq.com>
2018-12-15 17:14:11 +0800

    Initial commit

---
+++ src/main/java/com/cs/springbootestfives/SpringbootestfivesApplication.java
@@ -0,0 +1,14 @@
+package com.cs.springbootestfives;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootestfivesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootestfivesApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/cs/springbootestfives/SpringbootestfivesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cs.springbootestfives;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootestfivesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
